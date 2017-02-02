b9e1a898d7169bb2e04bbd61ce244d29fe582d15
Dimitri Marinho <dimitrimarinho@infojr.com.br>
2017-02-02 19:31:10 -0300

    Códigos 01 e 02 da Lista 04

---
+++ 4ª_lista/AERO.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+	int A, V, X, Y;
+		
+	int j=0, k, biggest_airport = 0;
+
+	cin >> A >> V;
+	
+	while (A!=0 && V!=0){
+
+		int quantityX[101], biggests[101];
+
+		for (int i=0; i < 101; i++){
+	
+			quantityX[i]=0;
+		
+			quantityX[i]=0;
+		
+		}
+	
+		//for (int i=0; i < 101; i++){
+	
+		//	biggests[i]=0;
+		
+		//}
+
+		for (int i=0; i < V; i++){
+		
+			cin >> X >> Y;
+			
+			quantityX[X]+=1;
+			
+			quantityX[Y]+=1;
+			
+		} 
+
+			for (k=1; k <= A; k++){
+			
+				if (quantityX[k] >= biggest_airport){
+				
+					biggest_airport = quantityX[k];
+					
+				//	biggests[k]= k;
+					
+				}
+				//else {
+				//	biggests[k]=0;
+				//}
+				
+			}
+		
+			cout << endl << "Teste " << j+1 << endl;
+			
+			for (int p=1; p <= A; p++){
+			
+				if (quantityX[p]==biggest_airport){
+				
+					cout << p << " ";
+					
+				}
+			}
+			
+			cout << endl;		
+				
+				
+		j++;
+	
+		biggest_airport = 0;
+
+		cin >> A >> V;
+	
+	}
+
+} 
---
+++ 4ª_lista/MINADO12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int N;
+	cin N;
+	spaces[N],mina[N];
+	for (int i=0; i < N; i++){
+		cin >> spaces[i];
+		if (spaces[i] == 1){
+
+		}
+	}
+
+
+
+
+
+
+
+}
