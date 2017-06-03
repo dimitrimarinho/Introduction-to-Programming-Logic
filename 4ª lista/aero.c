520b84c1dcecae94ecced4974e278455be4745c1
Dimitri Marinho <dimitrimarinho@infojr.com.br>
2017-06-02 23:10:59 -0300

    Aceito pelo SPOJ: aero.c Aeroporto

---
+++ 4ª lista/aero.c
@@ -0,0 +1,50 @@
+// http://br.spoj.com/problems/AERO/
+
+#include<stdio.h>
+int main(void){
+
+	int A, V, X, Y, test = 0;
+	scanf("%d%d", &A, &V);
+		
+	while(A != 0 && V != 0){
+	
+		int fluxo[101], m = 0; 
+		
+		for (int i = 0; i < 101; i++){
+				fluxo[i] = 0;
+		}
+		
+		for (int i = 0; i < V; i++){
+			
+			scanf("%d%d", &X, &Y);
+			fluxo[X] += 1;
+			fluxo[Y] += 1;
+		}
+		for (int i = 1; i <= A; i++){
+			
+			if (fluxo[i] >= m){
+	  			m = fluxo[i];
+	  		}
+	  	}
+		
+		printf("\nTeste %d\n", ++test);
+		
+		for (int i = 1; i <= A; i++){
+			
+			if (fluxo[i] == m){
+				printf("%d ", i);
+			}
+		}
+		printf("\n");
+		
+					
+		scanf("%d%d", &A, &V);
+		
+	}
+
+
+
+
+
+
+}
