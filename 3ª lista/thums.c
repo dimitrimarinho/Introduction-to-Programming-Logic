b3dd6308e8eaf8d15b606869c142cda24af56fa7
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-06-07 14:22:43 -0300

    Códigos da 3ª Lista de 2017.1  prontos

---
+++ 3ª lista/thums.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void){
+
+	int N;
+	scanf("%d", &N);
+	for (int i=1;  i <= N; i++){
+		if(i % 2 == 1){
+			printf("THUMS THUMS THUMS\n");
+		}
+		else{
+			printf("THUMS THUMS THUMS THUMS THUMS THUMS\n");
+		}
+	}
+
+
+
+}
