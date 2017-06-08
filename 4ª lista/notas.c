07713d8b2fa8f042078b61b135a8c09545a768dc
Dimitri Marinho <dimitrimarinho@infojr.com.br>
2017-06-07 21:25:51 -0300

    Aceito pelo SPOJ - notas.c

--- 4ª lista/notas.c
+++ 4ª lista/notas.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
 int main(void){
 
-	int N, a, A[100] = {}, m = 0;
+	int N, a, A[101] = {}, m = 0, M = 0;
 	scanf("%d", &N);
 	for (int i = 0; i < N; i++){
 		scanf("%d", &a);
 		A[a] += 1;
 	}
-	for (int j = 0; j < 100; j++){
-		if (A[j] >= A[j+1]){
-			m = A[j];
+	for (int i = 0; i <= 100; i++){
+		if (A[i] >= m){
+			m = A[i];
+			M = i;
 		}
-		else if (A[j] <= A[j+1]){
-			m = A[j+1];
-		}
-	}
-	printf("%d\n", m);
+	}			
+	printf("%d\n", M);
+		
 	
 }
