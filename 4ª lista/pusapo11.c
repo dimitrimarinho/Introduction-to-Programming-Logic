792172e3e30e68099c3fe888b0c2d4a3bf5c757c
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-06-08 23:09:47 -0300

    Aceito no SPOJ: Pulo do Sapo

---
+++ 4ª lista/pusapo11.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(void){
+
+	int N, M, P, D; // N e M representando o número de pedras no rio e o número de sapos, respectivamente
+			// Cada uma das M linhas seguintes possui dois inteiros P e D representando a posição inicial de um sapo e a distância fixa de pulo, respectivamente.
+	scanf("%d%d", &N, &M);
+	int vet[N];
+	for (int i = 0; i < N ; i++){
+		vet[i] = 0;
+	}
+	for (int i = 0; i < M ; i++){
+	
+		scanf("%d%d", &P, &D);	
+		vet[P-1]= 1;
+		for (int j = 1; j <= N; j++){
+			if ((P-1) + D * j >= 0 && (P-1) + D * j < N){
+				vet[(P-1) + D * j] = 1;
+				// printf("J POSITIVO , j é %d %d\n", j, vet[(P-1) + D * i] );
+			}
+			if ((P-1) - D * j >= 0 && (P-1) - D * j < N){
+				vet[(P-1) - D * j] = 1;
+				// printf("J NEGATIVO , j é %d %d\n", j , vet[(P-1) - D * i] );
+			}
+		}
+	}
+	for (int i = 0; i < N; i++){
+		printf("%d\n", vet[i]);
+	}
+
+
+
+
+
+}
