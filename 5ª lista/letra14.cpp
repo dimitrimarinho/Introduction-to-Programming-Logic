f36dc9e0c68334ef44d3b4cd82d0ee6c05dc3c70
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-06-14 23:11:41 -0300

    Aceito no SPOJ: Letras (Tentar refazer em C)

---
+++ 5ª lista/letra14.cpp
@@ -0,0 +1,30 @@
+	#include<iostream>
+	#include<string>
+	#include<iomanip>
+	using namespace std; 
+	int main(void){
+	
+		cout << fixed << setprecision(1);
+		char c;
+		string text;
+		int a = 1, p = 1, e = 0;
+		float r;
+		cin >> c;
+    		cin.ignore();
+	    	getline(cin, text);
+		for (int i = 0; i < text.size(); i++){
+			if (text[i] == ' '){
+				a++;
+				p = 1;
+			}
+			if (text[i] == c && p == 1){
+				e++;
+				p = 0;
+			}
+			
+				 
+		}
+		// cout << e << " " << a << endl;
+		r = ((float)e/a) * 100;
+		cout << r << endl;	
+		}
