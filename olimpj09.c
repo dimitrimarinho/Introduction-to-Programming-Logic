2e3beb35d449824acde87c2036042072e5e9c510
dimitrimarinho <dimitrimarinho@users.noreply.github.com>
2017-07-15 14:02:57 -0300

    PRECISA ACERTAR ESSA PORRA

--- olimpj09.c
+++ olimpj09.c
@@ -5,46 +5,49 @@ int main(void){
 
       int N, M, swap, swap2, ouro, prata, bronze, c , d, position, position2;
       scanf("%d%d", &N, &M);  
-      int pais[N][2];
+      int pais[2][N];
 
-     for (int i = 0; i < N; i++){
-     	pais[i][0] = -1;
+     for (int i = 0; i < 2; i++){
+	for (int j = 0; j < N; j++){
+     		pais[i][j] = -1;
+	}
       }
 
      for (int medals = 0; medals < M; medals++){
           scanf("%d%d%d", &ouro, &prata, &bronze);
-          pais[ouro-1][0]+=1;
-          pais[prata-1][0]+=1;
-          pais[bronze-1][0]+=1;
+          pais[0][ouro-1]+=1;
+          pais[0][prata-1]+=1;
+          pais[0][bronze-1]+=1;
      }
 
-     for (int i = 0; i < N; i++){
-          pais[i][1] = i+1;
-	  // printf("%d", pais[i][1]);
+     for (int p = 0; p < N; p++){
+          pais[1][p] = p+1;
+	  // printf("%d", pais[1][p]);
      }
      
-     	for (c = 0; c < N-1; c++){
-            position = c;
-            position2 = c;
-            for (d = c + 1; d < N; d++){
-              if (pais[position][0] < pais[d][0]){
-                position = d;
-                position2 = d;
-              }
-            }
-          if (position != c){
-              swap = pais[c][0];
-	      swap2 = pais[c][1];
-              printf("SWAP %d , SWAP2 %d", swap, swap2);
-              pais[c][0] = pais[position][0];
-              pais[c][1] = pais[position2][1];
-              pais[position][0] = swap;
-              pais[position2][1] = swap2;
+     for (c = 0; c < N-1; c++){
+           position = c;
+           position2 = c;
+           for (d = c + 1; d < N; d++){
+             if (pais[0][position] < pais[0][d]){
+               position = d;
+               position2 = d;
+             }
            }
-	    }
+         if (position != c){
+             swap = pais[0][c];
+	     swap2 = pais[1][c];
+             pais[0][c] = pais[0][position];
+             pais[1][c] = pais[1][position2];
+             pais[0][position] = swap;
+             pais[1][position2] = swap2;
+          }
+      }
       
       for (int i = 0; i < N; i++){
-            printf("%d ", &pais[i][1]);
+            printf("%d ", &pais[1][i]);
       }
+
+      printf("\n");	
 
 }
