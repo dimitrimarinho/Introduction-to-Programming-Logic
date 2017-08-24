0fae76082ce1beba494285741c12937a6de2b7de
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-08-23 23:13:24 -0300

    Investindo em ações: Aceito no SPOJ

---
+++ 9ª lista/acoes1mg.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int portfolio (int N, int K);
+int main(){
+
+	int N, K;
+	while (cin >> N >> K, N != 0 && K !=0){
+
+		cout << portfolio(N, K) << endl;
+	
+	}
+
+
+}
+
+int portfolio (int N, int K){
+
+	if (N > K){
+		if (N%2 == 0){
+			return 2*portfolio(N/2, K);
+		}
+		else{
+			return portfolio(N/2, K) + portfolio(N/2 + 1, K);
+		}
+	}
+	if (N <= K){
+		return 1;
+	}
+
+}
