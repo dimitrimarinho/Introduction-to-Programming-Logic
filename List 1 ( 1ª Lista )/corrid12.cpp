8ce09b46cbac85a438fb2c1d7f6044205b1c706d
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-10-04 23:52:45 -0300

    Adding new resolved code

---
+++ List 1 ( 1ª Lista )/corrid12.cpp
@@ -0,0 +1,12 @@
+// Link of the problem (language PT-BR): http://br.spoj.com/problems/CORRID12/
+// (Name of the problem) CORRID12 - Corrida
+
+#include<iostream>
+using namespace std;
+int main(void){
+
+    int C, N;
+    cin >> C >> N;
+    cout << (C%N) << endl;
+    
+}
