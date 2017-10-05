16ae2039b11a585f1c1316eb808812d2d053dd45
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-10-05 16:21:43 -0300

    Adding new resolved code

---
+++ List 1 ( 1ª Lista )/quadrad2.cpp
@@ -0,0 +1,12 @@
+// Link of the problem (language PT-BR): http://br.spoj.com/problems/QUADRAD2/
+// (Name of the problem) QUADRAD2 - Quadrados
+
+#include<iostream>
+using namespace std;
+int main(void){
+
+	int N;
+	cin >> N;
+	cout << N * N << endl;
+
+}
