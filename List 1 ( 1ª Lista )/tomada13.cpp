253ef43bd1dc02bdecc23abb93e73999c75c7b03
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-10-05 00:17:06 -0300

    Adding new resolved code

---
+++ List 1 ( 1ª Lista )/tomada13.cpp
@@ -0,0 +1,12 @@
+// Link of the problem (language PT-BR): br.spoj.com/problems/TOMADA13/
+// (Name of the problem) TOMADA13 - Tomadas
+
+#include<iostream>
+using namespace std;
+int main(void){
+
+  int t1,t2,t3,t4;
+  cin >> t1 >> t2 >> t3 >> t4;
+  cout << (t1+t2+t3+t4) - 3 << endl;
+  
+}
