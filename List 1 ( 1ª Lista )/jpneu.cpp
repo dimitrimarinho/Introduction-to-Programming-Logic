cc358a99deb51456ec2feaa1dcf21c4e8d61173f
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-10-04 23:44:37 -0300

    Adding new resolved code

---
+++ List 1 ( 1ª Lista )/jpneu.cpp
@@ -0,0 +1,12 @@
+// Link of the problem (language PT-BR): http://br.spoj.com/problems/JPNEU/
+// (Name of the problem) JPNEU - Pneu
+
+#include<iostream>
+using namespace std;
+int main(void){
+
+  int N, M;
+  cin >> N >> M;
+  cout << (N-M) << endl;
+
+}
