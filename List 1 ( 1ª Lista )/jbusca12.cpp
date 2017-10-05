4dc21fb6d35b0b85afa38b54bdf2ca7d95c1048d
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-10-05 00:01:56 -0300

    Adding new resolved code

---
+++ List 1 ( 1ª Lista )/jbusca12.cpp
@@ -0,0 +1,12 @@
+// Link of the problem (language PT-BR): http://br.spoj.com/problems/JBUSCA12/
+// (Name of the problem) JBUSCA12 - Busca na Internet
+
+#include<iostream>
+using namespace std;
+int main(void){
+
+    int t;
+    cin >> t;
+    cout << 4 * t << endl;
+
+}
