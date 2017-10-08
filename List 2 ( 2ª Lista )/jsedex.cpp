d69db47b2d470bf5427c23b696e97215816af341
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2017-10-07 22:06:55 -0300

    Adding new resolved code

---
+++ List 2 ( 2ª Lista )/jsedex.cpp
@@ -0,0 +1,16 @@
+// Link of the problem (language PT-BR): http://br.spoj.com/problems/JSEDEX/
+// (Name of the problem) JSEDEX - Sedex
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(void){
+
+  int N, A, L, P;
+  cin >> N >> A >> L >> P;
+  if ((A >= N) && (L >= N) && (P >= N))
+    cout << "S\n";
+  else
+    cout << "N\n";
+
+}
